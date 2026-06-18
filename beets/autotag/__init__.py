f0cb7ed2e29800657bb2d32f4efeddf98217ba7a
Šarūnas Nejus <snejus@protonmail.com>
2026-06-18 01:36:18 +0100

    Move AttrDict under beets.util

--- beets/autotag/__init__.py
+++ beets/autotag/__init__.py
@@ -22,7 +22,7 @@ from importlib import import_module
 from beets.util.deprecation import deprecate_for_maintainers, deprecate_imports
 
 from .distance import Distance, distance, string_dist, track_distance
-from .hooks import AlbumInfo, AttrDict, Info, TrackInfo, correct_list_fields
+from .hooks import AlbumInfo, Info, TrackInfo, correct_list_fields
 from .match import (
     AlbumMatch,
     Match,
@@ -48,7 +48,6 @@ def __getattr__(name: str):
 __all__ = [
     "AlbumInfo",
     "AlbumMatch",
-    "AttrDict",
     "Distance",
     "Info",
     "Match",
