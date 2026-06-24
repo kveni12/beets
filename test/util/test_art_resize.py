1d01450d54011086e58cd6b0e1bb40bb90e62ff8
Šarūnas Nejus <snejus@protonmail.com>
2026-06-24 23:27:03 +0100

    Move DummyIMBackend to fixtures

--- test/util/test_art_resize.py
+++ test/util/test_art_resize.py
@@ -23,21 +23,7 @@ from beets.test import _common
 from beets.test.helper import BeetsTestCase, CleanupModulesMixin
 from beets.util import command_output, syspath
 from beets.util.artresizer import IMBackend, PILBackend
-
-
-class DummyIMBackend(IMBackend):
-    """An `IMBackend` which pretends that ImageMagick is available.
-
-    The version is sufficiently recent to support image comparison.
-    """
-
-    def __init__(self):
-        """Init a dummy backend class for mocked ImageMagick tests."""
-        self.version = (7, 0, 0)
-        self.legacy = False
-        self.convert_cmd = ["magick"]
-        self.identify_cmd = ["magick", "identify"]
-        self.compare_cmd = ["magick", "compare"]
+from beets.test.fixtures import DummyIMBackend
 
 
 class ArtResizerFileSizeTest(CleanupModulesMixin, BeetsTestCase):
