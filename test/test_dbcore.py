2e650acf159ebb16a5f265dad09d3680bd1080ab
Šarūnas Nejus <snejus@protonmail.com>
2026-06-24 01:38:02 +0100

    Do not wrap single query with AndQuery

--- test/test_dbcore.py
+++ test/test_dbcore.py
@@ -631,9 +631,7 @@ class QueryFromStringsTest(unittest.TestCase):
 
     def test_zero_parts(self):
         q = self.qfs([])
-        assert isinstance(q, query.AndQuery)
-        assert len(q.subqueries) == 1
-        assert isinstance(q.subqueries[0], query.TrueQuery)
+        assert isinstance(q, query.TrueQuery)
 
     def test_two_parts(self):
         q = self.qfs(["foo", "bar:baz"])
@@ -644,15 +642,15 @@ class QueryFromStringsTest(unittest.TestCase):
 
     def test_parse_fixed_type_query(self):
         q = self.qfs(["field_one:2..3"])
-        assert isinstance(q.subqueries[0], query.NumericQuery)
+        assert isinstance(q, query.NumericQuery)
 
     def test_parse_flex_type_query(self):
         q = self.qfs(["some_float_field:2..3"])
-        assert isinstance(q.subqueries[0], query.NumericQuery)
+        assert isinstance(q, query.NumericQuery)
 
     def test_empty_query_part(self):
         q = self.qfs([""])
-        assert isinstance(q.subqueries[0], query.TrueQuery)
+        assert isinstance(q, query.TrueQuery)
 
 
 class SortFromStringsTest(unittest.TestCase):
@@ -712,9 +710,9 @@ class ParseSortedQueryTest(unittest.TestCase):
 
     def test_no_spaces_or_query(self):
         q, s = self.psq("foo,bar")
-        assert isinstance(q, query.AndQuery)
+        assert isinstance(q, query.OrQuery)
         assert isinstance(s, sort.NullSort)
-        assert len(q.subqueries) == 1
+        assert len(q.subqueries) == 2
 
     def test_trailing_comma_or_query(self):
         q, s = self.psq("foo , bar ,")
@@ -730,9 +728,8 @@ class ParseSortedQueryTest(unittest.TestCase):
 
     def test_only_direction(self):
         q, s = self.psq("-")
-        assert isinstance(q, query.AndQuery)
+        assert isinstance(q, query.NotQuery)
         assert isinstance(s, sort.NullSort)
-        assert len(q.subqueries) == 1
 
 
 class ResultsIteratorTest(unittest.TestCase):
