4b349efc21201848d3323be2acb6ac7e6d4bc826
Šarūnas Nejus <snejus@protonmail.com>
2026-06-24 12:43:44 +0100

    Make the global config fixture function-scoped

--- test/plugins/test_lastgenre.py
+++ test/plugins/test_lastgenre.py
@@ -281,16 +281,6 @@ class LastGenrePluginTest(IOMixin, PluginTestCase):
         )
 
 
-@pytest.fixture
-def config(config):
-    """Provide a fresh beets configuration for every test/parameterize call
-
-    This is necessary to prevent the following parameterized test to bleed
-    config test state in between test cases.
-    """
-    return config
-
-
 @pytest.mark.parametrize(
     "config_values, item_genre, mock_genres, expected_result",
     [
